c4d9265e908b91c9f2baf6d1ee80d155647aae72
ElectroCorp <chinmaytanmay@outlook.com>
2023-06-29 15:22:10 -0600

    MORE

---
+++ src/chase.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 Electro Corparation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
+
+#include "chase.h"
+
+/*
+ * The init function does something pretty wack. It inits (most helpful program comment)
+ * 
+ * but really it does:
+ *  > Thread manager inits
+ *  > Enviorment setup
+*/
+
+void Chase::init() {
+	plog::init(plog::debug, "log.txt");
+
+	PLOG_DEBUG << "Chase Init - Version [VERSIONNUM]";
+
+	
+}
