6bf709e207848570e8a0b197599cd612eb4c0cac
Electro-Corp <chinmaytanmay@outlook.com>
2023-07-11 12:31:56 -0600

    fixed another human generation issue (L)

--- src/threadmanager/threadmanager.cpp
+++ src/threadmanager/threadmanager.cpp
@@ -52,12 +52,12 @@ int32_t sub_loc = 0;
 
 Human::human_id ThreadManager::ThreadManager::createHuman(std::string name) {
 	if (sub_loc > gThreads[loc]->getSize() - 1) { ++loc; }
-	if (sub_loc > gThreads.size() - 1) {
+	if (loc > gThreads.size() - 1) {
 		PLOG_DEBUG << "Current GroupThread full, allocating a new one [(loc, sub_loc, gThreads.size()) = (" << loc << ", " << sub_loc << ", " << gThreads.size() << ")]";
 		// Allocate a new GroupThread if the latest one is full
 		GroupThread* tmp = new GroupThread(10); 
 		gThreads.push_back(tmp);
-		loc = 0;
+		sub_loc = 0;
 	}
 	// Create new human
 	Human::human_id id = { loc, sub_loc };
